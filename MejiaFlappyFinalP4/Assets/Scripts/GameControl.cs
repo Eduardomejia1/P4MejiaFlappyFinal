39a63f87f055058ac1e2121386b75e062e4e3804
Eduardomejia1 <730845@student.dusd.net>
2025-12-13 13:03:17 -0800

    Added obstacles
    Started adding obstacles.

--- MejiaFlappyFinalP4/Assets/Scripts/GameControl.cs
+++ MejiaFlappyFinalP4/Assets/Scripts/GameControl.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
 
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
     public float scrollspeed = -1.5f;
-    
+
+    private int score = 0;
+
     // Start is called before the first frame update
     void Awake ()
     {
@@ -24,12 +28,22 @@ public class GameControl : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        if (gameOver == true && Input.GetMouseButtonDown(0))
+        if (gameOver == true && Input.GetMouseButtonDown (0))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
         }
     }
-    
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString ();
+    }
+
     public void BirdDied()
     { 
         gameOverText.SetActive (true);
